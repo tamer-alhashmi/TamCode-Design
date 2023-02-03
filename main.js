db7be646c4780a768738ce079dfb5f1666ff2a63
tamer-alhashmi <tam.elhashmy@gmail.com>
2023-02-03 03:51:31 +0200

    #Stats-Sec_Counter>>JS

--- main.js
+++ main.js
@@ -1,12 +1,36 @@
 // Our Skills
-let section = document.querySelector(".our-skills");
+let skillsSction = document.querySelector(".our-skills");
 let skillSpans = document.querySelectorAll(".skills-bar span");
-// skillSpans.forEach.style.transition = "1.6s";
+
 window.onscroll = function () {
-  if (window.scrollY >= section.offsetTop) {
-    console.log("Reached Section Three");
+  if (window.scrollY >= skillsSction.offsetTop) {
     skillSpans.forEach((span) => {
       span.style.width = span.dataset.width;
     });
   }
 };
+
+// Start ourskills number animat
+
+let statSec = document.querySelector(".stats");
+let nums = document.querySelectorAll("span.records-count");
+let started = false;
+
+window.onscroll = function () {
+  if (window.scrollY >= statSec.offsetTop) {
+    if (!started) {
+      nums.forEach((num) => startCount(num));
+    }
+    started = true;
+  }
+};
+
+function startCount(el) {
+  let goal = el.dataset.goal;
+  let count = setInterval(() => {
+    el.textContent++;
+    if (el.textContent == goal) {
+      clearInterval(count);
+    }
+  }, 2000 / goal);
+}
