c0dde86c3ad2f0209dd24d849e2fc3c186d20ef8
TAMER ALHASHMI <111964090+tamer-alhashmi@users.noreply.github.com>
2023-02-07 22:33:30 +0200

    #Swiper error

--- js/main.js
+++ js/main.js
@@ -127,32 +127,40 @@ let counter = setInterval(() => {
 
 
 // Swiper Start
-let swiper = new Swiper(".slide-content", {
-  slidesPerView: 3,
-  spaceBetween: 25,
-  // slidesPerGroup: 3,
-  loop: true,
-  centerSlide: 'true',
-  fade: 'true',
-  grabCursor: 'true',
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    dynamicBullets: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  breakpoints: {
-    0: {
-      slidesPerView: 1,
-    },
-    520: {
-      slidesPerView: 2,
-    },
-    950: {
-      slidesPerView: 3,
-    },
-  },
-});
+// let swiper = new swiper(".mySwiper", {
+//   slidesPerView: "auto",
+//   spaceBetween: 25,
+//   // slidesPerGroup: 3,
+//   // loop: true,
+//   // centerSlide: "true",
+//   fade: "true",
+//   grabCursor: "true",
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//     dynamicBullets: true,
+//   },
+//   // navigation: {
+//   //   nextEl: ".swiper-button-next",
+//   //   prevEl: ".swiper-button-prev",
+//   // },
+//   breakpoints: {
+//     0: {
+//       slidesPerView: 1,
+//     },
+//     520: {
+//       slidesPerView: 2,
+//     },
+//     950: {
+//       slidesPerView: 3,
+//     },
+//   },
+// });
+    var swiper = new Swiper(".mySwiper", {
+      slidesPerView: "auto",
+      spaceBetween: 30,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
