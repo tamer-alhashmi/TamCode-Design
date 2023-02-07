f1d57d4d3cfd136b5e02240cfee747935a67b579
TAMER ALHASHMI <111964090+tamer-alhashmi@users.noreply.github.com>
2023-02-07 08:44:12 +0200

    2-7 fix testominial

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+
+
 let megaImgDiv = document.querySelector(".mega-Img");
 let megaImgPic = document.createElement("img");
 megaImgDiv.append(megaImgPic);
@@ -121,21 +123,36 @@ let counter = setInterval(() => {
 }, 1000);
 // Event Section Animation End
 
+
+
+
 // Swiper Start
 let swiper = new Swiper(".slide-content", {
   slidesPerView: 3,
   spaceBetween: 25,
   // slidesPerGroup: 3,
   loop: true,
-  fade: true,
-  grabCursor: true,
-  centerSlide: true,
+  centerSlide: 'true',
+  fade: 'true',
+  grabCursor: 'true',
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
+    dynamicBullets: true,
   },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    520: {
+      slidesPerView: 2,
+    },
+    950: {
+      slidesPerView: 3,
+    },
   },
 });
