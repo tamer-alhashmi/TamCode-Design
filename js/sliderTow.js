b1ebc15dd89af86cea7d0b5a1d81b6d645ad2a6a
tamer-alhashmi <tam.elhashmy@gmail.com>
2023-03-15 06:19:55 +0200

    Updat

--- js/sliderTow.js
+++ js/sliderTow.js
@@ -1,6 +1,6 @@
 // New Slider
 let sliderImage = Array.from(
-  document.querySelectorAll(".sliderZone .sliderOne")
+  document.querySelectorAll(".sliderZone img")
 );
 
 let sliderTitle = Array.from(
@@ -24,32 +24,15 @@ sliderCount = sliderImage.length;
 currentSlide = 1;
 checkFunction();
 
-// check Function
-function checkFunction() {
-  setInterval(() => {
-    removeAllActive();
 
-    sliderImage[currentSlide - 1].classList.add("active");
-    sliderTitle[currentSlide - 1].classList.add("active");
-    sliderMenuLi[currentSlide - 1].classList.add("active");
-    maskBg[currentSlide - 1].classList.add("active");
-    // mask.style.cssText =
-    //   "border-image: url(../photo/landingSlider/agriculture-iot-with-rice-field-background.jpg) 25 25;";
-
-    currentSlide++;
-    if (currentSlide > 4) {
-      currentSlide = 1;
-    }
-  }, 6000);
-}
 
 // Remove All Active Classes from all Elements
 function removeAllActive() {
   sliderImage.forEach(function (img) {
     img.classList.remove("active");
-  });
+});
   maskBg.forEach(function (msk) {
-    msk.classList.remove("active");
+      msk.classList.remove("active");
   });
   sliderTitle.forEach(function (titl) {
     titl.classList.remove("active");
@@ -57,4 +40,25 @@ function removeAllActive() {
   sliderMenuLi.forEach(function (lis) {
     lis.classList.remove("active");
   });
+}
+
+    //   checkFunction();
+
+    // check Function
+function checkFunction() {
+  setInterval(() => {
+      
+      removeAllActive();
+      sliderImage[currentSlide - 1].classList.add("active");
+      sliderTitle[currentSlide - 1].classList.add("active");
+      sliderMenuLi[currentSlide - 1].classList.add("active");
+      maskBg[currentSlide - 1].classList.add("active");
+    // mask.style.cssText =
+    //   "border-image: url(../photo/landingSlider/agriculture-iot-with-rice-field-background.jpg) 25 25;";
+    
+    currentSlide++;
+    if (currentSlide > 4) {
+      currentSlide = 1;
+    }
+  }, 6000);
 }
